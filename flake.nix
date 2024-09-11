552444876f2de0ff59f201b06687895fae468c13
nickrallison <nickrallison1@gmail.com>
2024-09-11 06:47:06 -0600

    init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+    description = "A flake for C development";
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    outputs = { self, nixpkgs }: {
+        devShells = {
+            x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShellNoCC {
+                packages = with pkgs; [
+                    cmake
+                ];
+                shellHook = ''
+                    
+                '';
+            };
+            aarch64-linux = with nixpkgs.legacyPackages.aarch64-linux; mkShellNoCC {
+                packages = with pkgs; [
+                    cmake
+                ];
+                shellHook = ''
+                    
+                '';
+            };
+            aarch64-darwin = with nixpkgs.legacyPackages.aarch64-darwin; mkShellNoCC {
+                packages = with pkgs; [
+                    cmake
+                ];
+                shellHook = ''
+                    
+                '';
+            };
+        };
+    };
+}
